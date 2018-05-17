091ff5f22031aeb68668d1741965d50498971af8
frankhart2017 <sdharchou@gmail.com>
2018-05-17 17:14:31 +0530

    Incomplete

---
+++ REGRESSION/MULTIPLE LINEAR REGRESSION/main.py
@@ -0,0 +1,21 @@
+# Importing the libraries
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, 0:4].values
+y = dataset.iloc[:, 4].values
+
+# Creating dummy variable for text values
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+#Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
